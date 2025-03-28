82eabc7c1790b9a88981478d7eb02ec30069f797
Thandolwethu <223162477@mycput.ac.za>
2025-03-28 09:12:54 +0200

    Added SponsorFactoryTest.java

---
+++ src/test/java/factory/SponsorFactoryTest.java
@@ -0,0 +1,35 @@
+package factory;
+   /*
+ SponsorFactroyTest.java
+ SponsorFactoryTest model class
+ Author : Thandolwethu p Mseleku(223162477)
+ Date:25 March 2025
+  */
+import domain.Sponsor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SponsorFactoryTest {
+
+    @Test
+    void createSponsor() {
+        Sponsor sponsor = SponsorFactory.createSponsor("Thandolwethu Mseleku", 200);
+        assertNotNull(sponsor);
+        System.out.println("Employee details:" + sponsor.toString());
+    }
+
+    @Test
+    void createSponsorWithInvalidName() {
+        Sponsor sponsor = SponsorFactory.createSponsor("", 200);
+        assertNull(sponsor);
+        System.out.println("Employee details:" + sponsor);
+    }
+
+    @Test
+    void createSponsorWithInvalidAmount() {
+        Sponsor sponsor = SponsorFactory.createSponsor("Thandolwethu Mseleku", -1);
+        assertNull(sponsor);
+        System.out.println("Employee details:" + sponsor);
+    }
+}
